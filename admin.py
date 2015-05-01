4174b87f4902b6a6e521fc5f682dd7da169908dc
Guilherme Guerra <gguerra@gguerra.local>
2015-05-01 16:37:54 -0300

    add rotas de user

--- admin.py
+++ admin.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'App python + flask - gguerra'
 
+@app.route('/users')
+def usuarios():
+    return 'Cadastro de usuários'
+
 if __name__ == '__main__':
     app.run()
