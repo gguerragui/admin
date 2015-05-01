498ba324279e9783bd984dd2130392cee176a1bd
gguerra <gguerragui@gmail.com>
2015-05-01 17:36:42 -0300

    implementando templates

--- admin.py
+++ admin.py
@@ -1,9 +1,15 @@
+from flask import render_template
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'App python + flask - gguerra'
+@app.route('/index')
+def index():
+    user = {'nickname': 'gguerra'}
+    render_template('index.html',
+                    title='Home',
+                    user=user)
 
 @app.route('/users')
 def usuarios():
